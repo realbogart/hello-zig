35e5b0baf0d6e58571a1f932fa0f8c064cac387c
Johan Yngman <johan.yngman@gmail.com>
2024-05-09 21:01:32 +0200

    Fix nix derivation

--- flake.nix
+++ flake.nix
@@ -9,25 +9,30 @@
       let
         pkgs = import nixpkgs { inherit system; };
         dependencies = with pkgs; [ zig raylib libGL ];
+        env = ''
+          export RAYLIB_PATH_INCLUDE=${pkgs.raylib}/include
+          export RAYLIB_PATH_LIB=${pkgs.raylib}/lib
+          export OPENGL_PATH_LIB=${pkgs.libGL}/lib
+          echo "RAYLIB_PATH_INCLUDE=$RAYLIB_PATH_INCLUDE"
+          echo "RAYLIB_PATH_LIB=$RAYLIB_PATH_LIB"
+          echo "OPENGL_PATH_LIB=$OPENGL_PATH_LIB"
+        '';
       in {
         packages.default = pkgs.stdenv.mkDerivation {
           name = "hello-zig";
           src = ./.;
           buildInputs = dependencies;
+          preConfigure = env;
+          dontUnpack = true;
           buildPhase = ''
-            zig build
+            cd $src
+            export HOME=$(mktemp -d)
+            zig build --prefix $out --cache-dir $HOME/zig-cache
           '';
         };
         devShells.default = pkgs.mkShell {
           buildInputs = dependencies;
-          shellHook = ''
-            export RAYLIB_PATH_INCLUDE=${pkgs.raylib}/include
-            export RAYLIB_PATH_LIB=${pkgs.raylib}/lib
-            export OPENGL_PATH_LIB=${pkgs.libGL}/lib
-            echo "RAYLIB_PATH_INCLUDE=$RAYLIB_PATH_INCLUDE"
-            echo "RAYLIB_PATH_LIB=$RAYLIB_PATH_LIB"
-            echo "OPENGL_PATH_LIB=$OPENGL_PATH_LIB"
-          '';
+          shellHook = env;
         };
       });
 }
